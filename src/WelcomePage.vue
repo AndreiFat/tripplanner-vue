515f201bbb30112f59787a5c0e7fa0e5684e1fd5
Andrei Fat <fatandrei57@gmail.com>
2024-12-07 20:12:55 +0200

    Small design update

--- src/WelcomePage.vue
+++ src/WelcomePage.vue
@@ -5,38 +5,38 @@
   <div class="container py-5">
     <h3 class="text-center">Welcome to Tripplanner Project!</h3>
     <b-row class="mt-5">
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center" href="/add_accommodations"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center" href="/add_accommodations"
                   variant="info">Add Accommodation
         </b-button>
       </b-col>
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center" href="/add_attractions"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center" href="/add_attractions"
                   variant="info">Add Attraction
         </b-button>
       </b-col>
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center"
                   href="/accommodations_detection" variant="info">Accommodation Detection
         </b-button>
       </b-col>
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center" href="/attraction_detection"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center" href="/attraction_detection"
                   variant="info">Attraction Detection
         </b-button>
       </b-col>
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center" href="/ride_info"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center" href="/ride_info"
                   variant="info">Ride Information
         </b-button>
       </b-col>
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center" href="/popups"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center" href="/popups"
                   variant="info">Popups
         </b-button>
       </b-col>
-      <b-col>
-        <b-button class="mb-3 w-100 h-100 d-flex align-items-center justify-content-center" href="/search"
+      <b-col class="mb-3">
+        <b-button class=" w-100 h-100 d-flex align-items-center justify-content-center" href="/search"
                   variant="info">Search Page
         </b-button>
       </b-col>
